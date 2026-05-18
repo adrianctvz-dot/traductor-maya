9f8a9567a1653df0f115f5c16f07b5719308021c
Adrian <adrianctvz@gmail.com>
2026-05-17 20:59:12 -0500

    separar funciones de traduccion

--- static/js/script.js
+++ static/js/script.js
@@ -4,18 +4,7 @@ let racha =
     Number(localStorage.getItem("racha")) || 1;
 
 
-async function traducir() {
-    const palabra = document.getElementById("palabra").value;
 
-    const respuesta = await fetch("/traducir", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            palabra: palabra
-        })
-    });
 
     const datos = await respuesta.json();
 
@@ -240,18 +229,7 @@ function actualizarAvatar(puntuacion) {
     hablarAvatar(mensajeAvatar.innerText);
 }
 
-async function traduccionIA() {
-    const palabra = document.getElementById("palabra").value;
 
-    const respuesta = await fetch("/traducir-ia", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            palabra: palabra
-        })
-    });
 
     const datos = await respuesta.json();
 
@@ -261,68 +239,6 @@ async function traduccionIA() {
 
     document.getElementById("palabra-practica").innerText = datos.resultado;
 }
-
-function nuevaPalabra() {
-    const palabras = [
-        "hola",
-        "gracias",
-        "comida",
-        "familia",
-        "escuela",
-        "amigo",
-        "casa",
-        "perro"
-    ];
-
-    const aleatoria =
-        palabras[Math.floor(Math.random() * palabras.length)];
-
-    document.getElementById("palabra").value = aleatoria;
-
-    traducir();
-}
-
-const categorias = {
-    basico: [
-        "hola",
-        "gracias",
-        "comida",
-        "tortilla"
-    ],
-
-    animales: [
-        "perro",
-        "gato",
-        "caballo",
-        "pollo"
-    ],
-
-    familia: [
-        "familia",
-        "mujer",
-        "hombre",
-        "niño"
-    ],
-
-    escuela: [
-        "escuela",
-        "libro",
-        "trabajo",
-        "amigo"
-    ]
-};
-
-function cargarCategoria(nombre) {
-    const lista = categorias[nombre];
-
-    const aleatoria =
-        lista[Math.floor(Math.random() * lista.length)];
-
-    document.getElementById("palabra").value = aleatoria;
-
-    traducir();
-}
-
 function hablarAvatar(texto) {
     const voz = new SpeechSynthesisUtterance(texto);
 
---
+++ static/js/traduccion.js
@@ -0,0 +1,82 @@
+async function traducir() {
+    const palabra = document.getElementById("palabra").value;
+
+    const respuesta = await fetch("/traducir", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            palabra: palabra
+        })
+    });
+    async function traduccionIA() {
+    const palabra = document.getElementById("palabra").value;
+
+    const respuesta = await fetch("/traducir-ia", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            palabra: palabra
+        })
+    });
+    function nuevaPalabra() {
+    const palabras = [
+        "hola",
+        "gracias",
+        "comida",
+        "familia",
+        "escuela",
+        "amigo",
+        "casa",
+        "perro"
+    ];
+
+    const aleatoria =
+        palabras[Math.floor(Math.random() * palabras.length)];
+
+    document.getElementById("palabra").value = aleatoria;
+
+    traducir();
+}
+const categorias = {
+    basico: [
+        "hola",
+        "gracias",
+        "comida",
+        "tortilla"
+    ],
+
+    animales: [
+        "perro",
+        "gato",
+        "caballo",
+        "pollo"
+    ],
+
+    familia: [
+        "familia",
+        "mujer",
+        "hombre",
+        "niño"
+    ],
+
+    escuela: [
+        "escuela",
+        "libro",
+        "trabajo",
+        "amigo"
+    ]
+};
+function cargarCategoria(nombre) {
+    const lista = categorias[nombre];
+
+    const aleatoria =
+        lista[Math.floor(Math.random() * lista.length)];
+
+    document.getElementById("palabra").value = aleatoria;
+
+    traducir();
+}
